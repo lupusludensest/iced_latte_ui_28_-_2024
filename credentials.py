3ea62194464e5631086cc26a9ff53fe0943cfbaa
Vic Gurov <gurovvic@gmail.com>
2024-03-28 19:59:12 -0400

    dt 28 march 2024

---
+++ credentials.py
@@ -0,0 +1,5 @@
+# credentials
+first_name = 'Viacheslav'
+last_name = 'Gurov'
+email_valid = 'gurovvic@gmail.com'
+password_valid = 'qwertyuiop12!@'
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,5 @@
 from pages.all_locators_bdd import *
+from credentials import *
 from pages.base_page import Page
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,20 +10,44 @@ import string
 
 class MainPage(Page):
 
-    # test_sendsafely_pytested_blue_top_1
-    # Click button "Accept" cookies
-    def accpt_ccks_btn(self):
+    # iced_latte_ui_register_new_user_1
+    # 1 Click user authorization icon
+    def clck_usr_authrztn_icn(self):
         wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(ACPT_CCKS_BTN)).click()
-    # End of the above code
-    # End of the above code
+        wait.until(EC.element_to_be_clickable(USER_AUTH_ICON)).click()
 
-    # 4 Verify "https://www.sendsafely.com/auth/#signup" is open
-    def vtf_sgn_p_url_pn(self):
-        self.driver.find_element(*GT_STRTD_BTN)
-        if self.driver.current_url == 'https://www.sendsafely.com/auth/#signup':
-            print(f'\nhttps://www.sendsafely.com/auth/#signup is here')
-        else:
-            print(f'\nhttps://www.sendsafely.com/auth/#signup is not here')
-        self.driver.back()
+    # 2 Click user registration button
+    def clck_usr_rgstrtn_btn(self):
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable(REGISTER_BTN)).click()
+
+    # 3 Send first name to first name field
+    def snd_frst_nm_to_fld(self):
+        first_name_field = self.driver.find_element(*FRST_NM_FLD)
+        first_name_field.clear()
+        first_name_field.send_keys(first_name)
+
+    # 4 Send last name to last name field
+    def snd_lst_nm_to_fld(self):
+        last_name_field = self.driver.find_element(*LST_NM_FLD)
+        last_name_field.clear()
+        last_name_field.send_keys(last_name)
+
+    # 5 Send email to email field
+    def snd_eml_to_fld(self):
+        email_field = self.driver.find_element(*EML_FLD)
+        email_field.clear()
+        email_field.send_keys(email_valid)
+
+    # 6 Send password to password field
+    def snd_pswrd_to_fld(self):
+        password_field = self.driver.find_element(*PSWRD_FLD)
+        password_field.clear()
+        password_field.send_keys(password_valid)
+
+    # 7 Click user register user button
+    def clck_usr_rgstr_usr_btn(self):
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable(REGISTER_USER_BTN)).click()
+
     # End of the above code
